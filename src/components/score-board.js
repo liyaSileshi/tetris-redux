32e2698cdd65bb206b98299d87b4147e11010953
liyaSileshi <liyasileshi@gmail.com>
2020-06-05 21:53:58 -0700

    added connection for score board

--- src/components/score-board.js
+++ src/components/score-board.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import {pause, resume, restart} from '../actions'
+
 class ScoreBoard extends Component {
   render() {
+    const {score, isRunning, gameOver, resume, restart, pause} = this.props
+
     return (
       <div className='score-board'>
-        <div>Score: {this.props.score}</div>
+        <div>Score: {score}</div>
         <div>Level: 1</div>
         <button className='score-board-button' onClick={(e)=>{
-        }}>Play</button>
+          if(gameOver) {return} // do nothing and return
+          isRunning ? pause() : resume() //call either based of isRunning
+
+        }}>{isRunning ? 'Pause' : 'Play'}</button>
         <button className='score-board-button' onClick={(e)=>{
+          restart()
         }}>Restart</button>
       </div>
     )
   }
 }
 
-export default ScoreBoard;
+const mapStateToProps = (state) => {
+  return {
+    score : state.game.score,
+    isRunning : state.game.isRunning,
+    gameOver : state.game.gameOver
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    pause,
+    resume,
+    restart
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps())(ScoreBoard);
