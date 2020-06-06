b18e8dd0d0529f73c71fdf1408611cff6ee6ca30
liyaSileshi <liyasileshi@gmail.com>
2020-06-05 17:18:31 -0700

    added state utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,165 @@ export const random = (min, max) => {
   // random number between min and max, starting from min
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+// returns the default grid
+export const gridDefault = () => {
+  const rows = 18
+  const cols = 10
+  const array = []
+  // Fill array with 18 arrays each containing
+  // 10 zeros (0)
+  for (let row = 0; row < rows; row++) {
+    // make an array for each row
+    array.push([])
+    for (let col = 0; col < cols; col++) {
+      array[row].push(0)
+    }
+  }
+  return array
+}
+
+// Define block shapes and their rotations as arrays.
+export const shapes = [
+  // none
+  [[[0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]]],
+
+  // I
+  [[[0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0]]],
+
+  // T
+  [[[0,0,0,0],
+    [1,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]]],
+
+  // L
+  [[[0,0,0,0],
+    [1,1,1,0],
+    [1,0,0,0],
+    [0,0,0,0]],
+
+   [[1,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,0,1,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,0,0]]],
+
+  // J
+  [[[1,0,0,0],
+    [1,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]],
+
+   [[0,1,1,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,0,0,0]],
+
+   [[0,0,0,0],
+    [1,1,1,0],
+    [0,0,1,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,0,0],
+    [1,1,0,0],
+    [0,0,0,0]]],
+
+  // Z
+  [[[0,0,0,0],
+    [1,1,0,0],
+    [0,1,1,0],
+    [0,0,0,0]],
+
+   [[0,0,1,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [0,0,0,0]]],
+
+  // S
+  [[[0,0,0,0],
+    [0,1,1,0],
+    [1,1,0,0],
+    [0,0,0,0]],
+
+   [[0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,0,0]]],
+
+  // O
+  [[[0,1,1,0],
+    [0,1,1,0],
+    [0,0,0,0],
+    [0,0,0,0]]]
+]
+
+// generate random shapes
+// return the index of a random shape from 1
+// to number of items in 'shape'
+// we start from one b/c don't need empty shape (first array)
+export const randomShape = () => {
+  return random(1, shapes.length - 1)
+}
+
+// return the default state of the game
+export const defaultState = () => {
+  return {
+    // create an empty grid
+    grid: gridDefault(),
+    // get a new random shape
+    shape: randomShape(),
+    // set rotation of the shape to 0
+    rotation: 0,
+    // set the 'x' position of the shape to 5 and y to -4, which
+    // puts the shape in thecenter of the gird above the top
+    x: 5,
+    y: -4,
+    // set the index of the next shape to new random shape
+    nextShape: randomShape(),
+    // tell the game that it's currently running
+    isRunning: true,
+    // set the score to 0
+    score: 0,
+    // set the default speed
+    speed: 1000,
+    // game isn't over yet
+    gameOver: false
+  }
+}
