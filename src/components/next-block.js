de49103bef1a68e309c2b13cc7f7282b6b9384fc
liyaSileshi <liyasileshi@gmail.com>
2020-06-05 17:54:42 -0700

    added connection for next block

--- src/components/next-block.js
+++ src/components/next-block.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 import GridSquare from './grid-square'
+import {connect} from 'react-redux'
+import { shapes } from '../utils'
 
 // draws the next block view showing the next block to drop
 class NextBlock extends Component {
   makeGrid() {
-    const box = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    // deconstruct shape
+    const {shape} = this.props
+    // get the array for this shape first rotation
+    const block = shapes[shape][0]
+    // get the empty shape
+    const box = shapes[0][0]
+
     // map the block to the grid
     return box.map((rowArray, row) => {
       return rowArray.map((square, col) => {
-        return <GridSquare key={`${row}${col}`} color={0}/>
+        // if there is a 1 color, use the shape index
+        // color is the index of the shape
+        const color = block[row][col] === 0 ? 0 : shape
+        return <GridSquare key={`${row}${col}`} color={color}/>
       })
     })
   }
@@ -22,4 +33,13 @@ class NextBlock extends Component {
   } 
 }
 
-export default NextBlock;
+// map state to props
+const mapStateToProps = (state) => {
+  return {
+    // return nextShape as shape
+    // shape is a random number (1, shapes.length)
+    shape: state.game.nextShape
+  }
+}
+
+export default connect(mapStateToProps)(NextBlock);
